d4ce00263210914147bab6f51ebd18d2623770a7
Raymond Finzel <prometheoid@gmail.com>
2013-07-21 00:01:08 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,90 @@
+ conflate
+===========
+
+A late-to-the-party, lightweight, stupid simple configuration manager.
+
+ Install
+----------
+
+`python setup.py install` should do it.
+
+
+ Use Cases
+-----------
+
+1. Create a dictionary full of the properties you'll want to be able to 
+load from a file.
+   ```python
+   
+   config = { height : 600,
+              width : 960,
+			  database_location : '/var/databas/foo',
+			  pages : ['index.html', 'upload.html', 'template.html'],
+			  preferences : None
+			}
+			
+	```
+	Yes, you may put anything you want as a value. These will probably be 
+	your defaults. Put `None` if you don't want the property to have a 
+	default.
+	
+	
+2. You can create a confmgr object with a configuration specified, or just
+   give it the filename(required) and wait until later. Configuration needs to
+   be in the working directory and will be created (by a read or write) 
+   if not present.
+   
+   ```python
+   
+   from conflate import confmgr
+   
+   c = confmgr('test.conf', config) # from above
+
+   ```
+   
+3. And now you can write to disk with `writeconf`, read from disk 
+   with `readconf`, and check your configuration with `printconf`.
+   
+   ```python
+   
+   c.writeconf() # writes out initial configuration
+   c.readconf() # sync contents of CONF with disk
+   c.printconf() # is everything okay?
+   
+   
+   ```
+   
+4. Changing properties is as easy as editing a dictionary.
+
+   ```python
+   
+   c.CONF['width'] = 700 # update an exisiting one
+   c.CONF['comment'] = 'This is a test' # and make a new one
+   c.writeconf() # ...and write everything to disk.
+   
+   ```
+ Options   
+--------------
+
++ confmgr can take named attributes:
+ + assign_op - change the assignment operator to whatever you please. 
+   Defaults to '='.
+   e.g. '=' for `test = 1` or ':' for `test : 1`
+ + comment_op - change comment symbol to whatever you please.
+   Defaults to '#'
+   e.g. '#' for `# is a comment` or ';' for `; is a comment`
+ + silent - a boolean that will silence any prompts
+ + CONF - the property that contains the configuration dict. Can be assigned
+   at object creation as an attribute, or directly later.
+
+
+ If I were you
+---------------
+
+Ship a configuration file with your product, or write a script to set it up 
+the first time. This way you can include comments and defaults that can be 
+changed later.
+
+An example configuration file can be found in the source. It is named 
+'test.conf'.
+
---
+++ conflate.py
@@ -0,0 +1,186 @@
+import ast
+import os
+import sys
+import pprint
+
+#Accepted commandline reactions to Y/N questions
+AFF = ["yes","y","yes'm"]
+NEG = ["no","n","nope"]
+
+
+
+class confmgr(object):
+
+    def __init__(self, config_filename, 
+                 assign_op = "=", comment_op = "#",
+                 silent = False, CONF = None):
+
+        """ Initialize the confmgr object.
+
+        Required Arguments:
+        config_filename -- string name of the file where configuration is
+                           or should be stored.
+
+        Keyword Arguments:
+        assign_op -- user defined assignment operator used in the 
+                     configuration file.
+        comment_op -- user defined comment operator used in the 
+                      configuration file.
+
+        NOTE: currently there is no way to escape the operators.
+
+        silent -- True to run module silently
+
+        """
+
+        self.config_filename = config_filename #string
+        self.assign_op = assign_op #string
+        self.comment_op = comment_op #string
+        self.silent = silent #boolean
+        self._CONF = None
+        self.CONF = CONF #Dictionary of configuration settings
+
+    def readconf(self):
+
+        """ Reads configuration from file.
+
+        Must have a config file on disk, or call to readconf will prompt user
+        to touch the file.
+        
+        A well formed configuration file will contain line separated 
+        key-value pairs of the form 'key `assign_op` value'.
+        
+        NOTE: Currently no support for multi-line values.
+
+        e.g. 'width = 960' , or 'names : [Ray, Augusta, Sally, Mitch]'
+
+
+        """
+
+        try:
+            f =  open(self.config_filename, 'r+')
+            config_file = f.read()
+            for k in self.CONF:
+                for t in config_file.splitlines():
+                    if self.comment_op in t:
+                        t = t[:t.find(self.comment_op)]
+                    p = t.partition(self.assign_op)
+                    if k == p[0].strip():
+                        try:
+                            self.CONF[k] = ast.literal_eval(p[2].strip())
+                        except ValueError:
+                            print >> sys.stderr, \
+                                "CONFLATE: Malformed value for property \'" + \
+                                str(k) + "\'"
+        except IOError as e:
+            self.nofile()
+
+
+    def writeconf(self):
+
+        """ Write changes from self.CONF to disk.
+
+        Creates file if no file is present, prompting in the same manner
+        as the `readconf` method. Compares self.CONF to the contents of
+        the configuration file; changes values if a difference is present,
+        inserts currently missing key-value pairs.
+
+        NOTE: Currently no way to write a single property at a time.
+        
+        """
+
+        try:
+            f =  open(self.config_filename, 'r+')
+            config_file = f.read()
+            f.close()
+            lines = config_file.splitlines()
+            for k in self.CONF:
+                exists = False
+                p = None
+                pprime = None
+                head = ''
+                tail = ''
+
+                for n,t in enumerate(lines[:]):
+                    if self.comment_op in t:
+                        head = t[:t.find(self.comment_op)]
+                        tail = t[t.find(self.comment_op):]
+                        p = head.partition(self.assign_op)
+                    else:
+                        tail = ''
+                        p = t.partition(self.assign_op)
+                    if k == p[0].strip():
+                        exists = True
+                        break
+
+                pprime = (str(k), self.assign_op, repr(self.CONF[k]))
+                if exists == True:
+                    lines[n] = ' '.join(pprime) + ' ' + tail
+                else:
+                    lines.append(' '.join(pprime))
+            f =  open(self.config_filename, 'w')
+            f.write('\n'.join(lines))
+
+        except IOError as e:
+            self.nofile()
+        
+
+    def printconf(self):
+        
+        """ Prints configuration (self.CONF) in an advantageous manner.
+
+        Takes no arguments.
+
+        """
+
+        pp = pprint.PrettyPrinter(indent=2)
+        pp.pprint(self.CONF)
+
+    
+    ####### Internals
+
+    @property
+    def CONF(self):
+        return self._CONF
+
+    @CONF.setter
+    def CONF(self, raw):
+        d = None
+        try:
+            if isinstance(raw,list):
+                d = dict.fromkeys(raw)
+            elif isinstance(raw,dict):
+                d = raw
+        except TypeError:
+            print >> sys.stderr, \
+                "CONFLATE: Configuration was not in an acceptable format."
+        finally:
+            self._CONF = d
+
+    def nofile(self):
+        # Handling for the absence of a configuration file
+        feedback = "Yes"
+        if not self.silent:
+            feedback = \
+                raw_input("No config file found. Touch \'" +
+                          self.config_filename+"\' in " + os.getcwd() + "? ")
+        if feedback.lower() in AFF:
+            print >> sys.stderr, \
+                "CONFLATE: No config file found. Touching \'" + \
+                self.config_filename+"\' in " + os.getcwd()
+            open(self.config_filename,'a+').close()
+
+        else:
+            sys.exit()
+
+
+if __name__=="__main__":
+    c = confmgr("test.conf", silent = True, CONF = ['test1','test2','test3'])
+    c.readconf()
+    c.printconf()
+    c.CONF = {'test1': False, 
+              'test2' : {'baz':['bye', 'bye', 'bye'], 'hat': 1, 'flibber':7},
+              'test3' : 'string'}
+    c.writeconf()
+    c.readconf()
+    c.printconf()
